6896e7ed2b9398ba04595d9675b91a5fde4a2262
Bosco H <y95han@gmail.com>
2018-09-24 20:43:59 -0400

    OS extension fn fini

--- Lab1/HelloWorld/helloworld.c
+++ Lab1/HelloWorld/helloworld.c
@@ -1,3 +1,244 @@
+// #include <LPC17xx.h>
+// #include "uart_polling.h" 
+// #include <RTL.h>
+// #include "../RTX_CM3/INC/RTL_ext.h" /* extended interface header file */
+// #include <stdio.h>
+// #include <string.h>
+
+// #define NUM_FNAMES 7
+
+// struct func_info {
+//   void (*p)();      /* function pointer */
+//   char name[16];    /* name of the function */
+// };
+
+// extern void os_idle_demon(void);
+// __task void task1(void);
+// __task void task2(void);
+// __task void task3(void);
+// __task void task4(void);
+// __task void task5(void);
+// __task void init (void);
+// __task void test_task(void);
+//  
+// char *state2str(unsigned char state, char *str);
+// char *fp2name(void (*p)(), char *str);
+
+// OS_MUT g_mut_uart;
+// OS_TID g_tid = 255;
+
+// int  g_counter = 0;  // a global counter
+// char g_str[16];
+// char g_tsk_name[16];
+
+// struct func_info g_task_map[NUM_FNAMES] = \
+// {
+//   /* os_idle_demon function ptr to be initialized in main */
+//   {NULL,  "os_idle_demon"}, \
+//   {task1, "task1"},   \
+//   {task2, "task2"},   \
+// 	{task3, "task3"},   \
+//   {task4, "task4"},   \
+//   {task5, "task5"},   \
+//   {init,  "init" }
+// };
+
+// /* no local variables defined, use one global var */
+// __task void task1(void)
+// {
+// 	printf("Task 1 still active!\n");
+// 	for (;;) {
+// 		g_counter++;
+// 	}
+// }
+
+
+// /*--------------------------- task2 -----------------------------------*/
+// /* checking states of all tasks in the system                          */
+// /*---------------------------------------------------------------------*/
+// __task void task2(void)
+// { 
+// 	for(;;) {
+// 		U8 i=1;
+// 		RL_TASK_INFO task_info;
+// 		int j = 0;
+// 		
+// 		os_mut_wait(g_mut_uart, 0xFFFF);
+// 		printf("TID\tNAME\t\tPRIO\tSTATE   \t%%STACK\n");
+// 		os_mut_release(g_mut_uart);
+// 			
+// 		for(i = 0; i <= os_tsk_count_get(); i++) {
+// 			if (os_tsk_get(i+1, &task_info) == OS_R_OK) {
+// 					os_mut_wait(g_mut_uart, 0xFFFF);  
+// 					printf("%d\t%s\t\t%d\t%s\t%d%%\n", \
+// 								 task_info.task_id, \
+// 								 fp2name(task_info.ptask, g_tsk_name), \
+// 								 task_info.prio, \
+// 								 state2str(task_info.state, g_str),  \
+// 								 task_info.stack_usage);
+// 					os_mut_release(g_mut_uart);
+// 			} 
+// 		}
+// 			
+// 		if (os_tsk_get(0xFF, &task_info) == OS_R_OK) {
+// 			printf("%p", task_info.ptask);
+// 				os_mut_wait(g_mut_uart, 0xFFFF);  
+// 				printf("%d\t%s\t\t%d\t%s\t%d%%\n", \
+// 							 task_info.task_id, \
+// 							 fp2name(task_info.ptask, g_tsk_name), \
+// 							 task_info.prio, \
+// 							 state2str(task_info.state, g_str),  \
+// 							 task_info.stack_usage);
+// 				os_mut_release(g_mut_uart);
+// 		}
+// 		//Delay the program
+// 		while (j < 1000000) {
+// 			j = j + 1;
+// 		}
+// 	}
+// }
+
+// void task4() {
+// 	printf("Task 4 Completed but still Active!\n");
+// 	for(;;);
+// }
+
+// void task5() {
+// 	printf("Task 5 Idly Waiting.\n");
+// 	for(;;){
+// 		os_dly_wait(1000);
+// 	}
+// }
+
+// void task3() {
+// 	printf("\nNumber of Active Tasks:%d\n", os_tsk_count_get());
+// 	printf("Task 3 Completed but still Active!\n");
+// 	for(;;);
+// }
+
+
+// /*--------------------------- init ------------------------------------*/
+// /* initialize system resources and create other tasks                  */
+// /*---------------------------------------------------------------------*/
+// __task void init(void)
+// {
+// 	os_mut_init(&g_mut_uart);
+//   
+// 	os_mut_wait(g_mut_uart, 0xFFFF);
+// 	printf("init: TID = %d\n", os_tsk_self());
+// 	os_mut_release(g_mut_uart);
+//   
+// 	g_tid = os_tsk_create(task1, 1);  /* task 1 at priority 1 */
+// 	os_mut_wait(g_mut_uart, 0xFFFF);
+// 	printf("init: created task1 with TID %d\n", g_tid);
+// 	os_mut_release(g_mut_uart);
+//   
+// 	g_tid = os_tsk_create(task2, 1);  /* task 2 at priority 1 */
+// 	os_mut_wait(g_mut_uart, 0xFFFF);
+// 	printf("init: created task2 with TID %d\n", g_tid);
+// 	os_mut_release(g_mut_uart);
+// 	
+// 	g_tid = os_tsk_create(task3, 1);  /* task 3 at priority 1 */
+// 	os_mut_wait(g_mut_uart, 0xFFFF);
+// 	printf("init: created task3 with TID %d\n", g_tid);
+// 	os_mut_release(g_mut_uart);
+// 	
+// 	g_tid = os_tsk_create(task4, 1);  /* task 4 at priority 1 */
+// 	os_mut_wait(g_mut_uart, 0xFFFF);
+// 	printf("init: created task4 with TID %d\n", g_tid);
+// 	os_mut_release(g_mut_uart);
+// 	
+// 	g_tid = os_tsk_create(task5, 1);  /* task 5 at priority 1 */
+// 	os_mut_wait(g_mut_uart, 0xFFFF);
+// 	printf("init: created task5 with TID %d\n", g_tid);
+// 	os_mut_release(g_mut_uart);
+//   
+// 	os_tsk_delete_self();     /* task MUST delete itself before exiting */
+// }
+
+// /**
+//  * @brief: convert state numerical value to string represenation      
+//  * @param: state numerical value (macro) of the task state
+//  * @param: str   buffer to save the string representation, 
+//  *               buffer to be allocated by the caller
+//  * @return:the string starting address
+//  */
+// char *state2str(unsigned char state, char *str)
+// {
+// 	switch (state) {
+// 	case INACTIVE:
+// 		strcpy(str, "INACTIVE");
+// 		break;
+// 	case READY:
+// 		strcpy(str, "READY   ");
+// 		break;
+// 	case RUNNING:
+// 		strcpy(str, "RUNNING ");
+// 		break;
+// 	case WAIT_DLY:
+// 		strcpy(str, "WAIT_DLY");
+// 		break;
+// 	case WAIT_ITV:
+// 		strcpy(str, "WAIT_ITV");
+// 		break;
+// 	case WAIT_OR:
+// 		strcpy(str, "WAIT_OR");
+// 		break;
+// 	case WAIT_AND:
+// 		strcpy(str, "WAIT_AND");
+// 		break;
+// 	case WAIT_SEM:
+// 		strcpy(str, "WAIT_SEM");
+// 		break;
+// 	case WAIT_MBX:
+// 		strcpy(str, "WAIT_MBX");
+// 		break;
+// 	case WAIT_MUT:
+// 		strcpy(str, "WAIT_MUT");
+// 		break;
+// 	default:
+// 		strcpy(str, "UNKNOWN");    
+// 	}
+// 	return str;
+// }
+
+// /** 
+//  * @brief: get function name by function pointer
+//  * @param: p the entry point of a function (i.e. function pointer)
+//  * @param: str the buffer to return the function name
+//  * @return: the function name string starting address
+//  */
+// char *fp2name(void (*p)(), char *str)
+// {
+// 	int i;
+// 	unsigned char is_found = 0;
+//   
+// 	for ( i = 0; i < NUM_FNAMES; i++) {
+// 		if (g_task_map[i].p == p) {
+// 			str = strcpy(str, g_task_map[i].name);  
+// 			is_found = 1;
+// 			break;
+// 		}
+// 	}
+// 	if (is_found == 0) {
+// 		strcpy(str, "ghost");
+// 	}
+// 	return str;
+// }
+
+// int main(void)
+// {
+// 	SystemInit();         /* initialize the LPC17xx MCU */
+// 	uart0_init();         /* initilize the first UART */
+//   
+//   
+// 	/* fill the fname map with os_idle_demon entry point */
+// 	g_task_map[0].p = os_idle_demon;
+//   
+// 	printf("Calling os_sys_init()...\n");
+// 	os_sys_init(init);    /* initilize the OS and start the first task */
+// }
+
 #include <LPC17xx.h>
 #include <RTL.h>
 #include <stdio.h>
@@ -10,13 +251,16 @@
 __task void task1()
 {
 	unsigned int i = 0;
-	int count;
 	for(;;i++)
 	{
-		
-		count = rt_tsk_count_get(); 
-		printf("Count: %d\n",count);
+		RL_TASK_INFO taskInfo;
+		os_tsk_get(2, &taskInfo);
+		//RUNNING
 		printf("Task1: %d\n", i);
+		//just checking that its running from within the task------------------------------------------------------
+		printf("Task 1 state: %d\n", taskInfo.state);
+		// --------------------------------------------------------------------------------------------------------
+		//delay
 		#ifdef MYSIM
 		os_dly_wait(1);
 		#else
@@ -27,35 +271,64 @@ __task void task1()
 
 __task void task2()
 {
-	//OS_RESULT testGet;
-	// = //malloc(64); //= malloc(sizeof(RL_TASK_INFO));
-	//int* ptr1 = (int*)malloc(sizeof(int));
-	
 	RL_TASK_INFO taskInfo;
 	
 	while(1)
 	{
 		int i;
 		int count = os_tsk_count_get();
-		printf("Task2: HelloWorld!\n");
+		printf("Active Tasks:%d\n",count);
+		printf("Task2: STATES\n");
 		
-		for(i = 0; i< count; i++)
+		for(i = 2; i< (count + 2); i++)
 		{
-			if(os_tsk_get(i+1, &taskInfo) == OS_R_OK)
+			if(os_tsk_get(i, &taskInfo) == OS_R_OK)
 			{
 				if(taskInfo.state == RUNNING)
 				{
-					printf("running\n");
+					printf("Task %d: running\n", i-1);
+				}
+				else
+				{
+					printf("Task %d: not running\n", i-1);
 				}
 			}
-			//os_tsk_get(i+1, &taskInfo);
-			
-			//if(taskInfo.state == RUNNING)
-			//	{
-			//		printf("running\n");
-			//	}
+			else
+			{
+				printf("Task %d: not okay\n", i-1);
+			}
 		}
 		
+		//delay
+		#ifdef MYSIM
+		os_dly_wait(3);
+		#else
+		os_dly_wait(300);
+		#endif
+	}
+}
+
+__task void task3()
+{
+	while(1)
+	{
+		printf("I am task 3\n");
+		
+		//delay
+		#ifdef MYSIM
+		os_dly_wait(3);
+		#else
+		os_dly_wait(300);
+		#endif
+	}
+}
+
+__task void task4()
+{
+	
+	while(1)
+	{
+		//delay
 		#ifdef MYSIM
 		os_dly_wait(3);
 		#else
@@ -68,6 +341,8 @@ __task void init(void)
 {
 	os_tsk_create(task1, 1); //task 1, priority 1
 	os_tsk_create(task2, 1); //task 2, priority 1
+	os_tsk_create(task3, 1); //task 3, priority 1
+	os_tsk_create(task4, 1); //task 3, priority 1
 	os_tsk_delete_self(); //delete itself before exiting
 }
 
--- Lab1/RTX_CM3/SRC/CM/rt_Task_ext.c
+++ Lab1/RTX_CM3/SRC/CM/rt_Task_ext.c
@@ -15,15 +15,13 @@
 // 
 int rt_tsk_count_get (void) {
 		// add your own code here
-		/* specifies the maximum number of tasks that can be active at the same time. 
+		/* os_maxtaskrun specifies the maximum number of tasks that can be active at the same time. 
 			 This includes tasks in any state (running, waiting, or ready) other than the INACTIVE state.*/
-		
-		//P_TCB *current = os_active_TCB;
-		
+	
 	  int i;
 		int activeTasks = 0;
 		P_TCB current = *os_active_TCB;
-	
+		
 		for(i=0; i < os_maxtaskrun; i++)
 			{
 				if(current != NULL && current->state != INACTIVE)
@@ -33,22 +31,25 @@ int rt_tsk_count_get (void) {
 				
 				current = os_active_TCB[i+1];
 			}
+			
 	return activeTasks;
 }
 
 /*----------------------- rt_tsk_get --------------------------------*/
 	OS_RESULT rt_tsk_get (OS_TID task_id, RL_TASK_INFO *p_task_info) {
+		// add your own code here		
 		U32 total_stack_size;
 		U32 task_occupied_size = 0;
-		
-		// add your own code here
+	
+		//get current task from TCB which starts from index 0
 		P_TCB task_content = os_active_TCB[task_id - 1];
 		
+		//invalid parameters
 		if(task_content == NULL || task_id > os_maxtaskrun)
 		{
 			return(OS_R_NOK);
 		}
-		
+			//fill p_task_info with all the information
 			p_task_info -> task_id = task_id ;
 			p_task_info -> state = task_content -> state; 
 			p_task_info -> prio = task_content -> prio;
@@ -58,45 +59,25 @@ int rt_tsk_count_get (void) {
 		
 		//stack usage
 		if(task_content -> state == RUNNING)
-		{
-			//task_occupied_size = (U32)rt_get_PSP() - (U32)task_content->stack;
+		{			
+			//PSP points to the last item added in stack
+			//task_content->stack points to the starting location of the task stack
 			
-			//PSP points to the last item in stack
-			//task_content->stack points to the starting location of the task in the stack
-			//PSP - task_content->stack is the space unused by that task
-			//total_stack_size - unused stack space = space occupied by task
-			task_occupied_size = total_stack_size - ((U32)rt_get_PSP() - (U32)task_content->stack);
+			//task_content -> stack + total_stack_size gives us the last address of the stack
+			//then subtracting PSP from the last address gives us size of the used space
+			task_occupied_size = (U32)(task_content -> stack) + total_stack_size - (U32)(rt_get_PSP());
 		}
 		else
 		{
-			task_occupied_size = total_stack_size - ((U32)task_content->tsk_stack - (U32)task_content->stack);
+			task_occupied_size = (U32)(task_content -> stack) + total_stack_size - (U32)(task_content->tsk_stack);
 		}
-		
-// 		p_task_info -> stack_usage = (U8) ((task_occupied_size / total_stack_size)*100);
 		  
-		  // percentage of used / total
-		  p_task_info -> stack_usage = (U8)((task_occupied_size/ total_stack_size)*100);
+		// store stack percentage of used / total
+		p_task_info -> stack_usage = (U8)((task_occupied_size*100)/ total_stack_size);
 		
 				
 		return OS_R_OK;
 		
-// 		P_TCB task_content = os_active_TCB[task_id - 1];
-// 		if(task_content != NULL)
-// 		{
-// 			
-// 			p_task_info -> task_id = task_id ;
-// 			p_task_info -> state = task_content -> state; 
-// 			p_task_info -> prio = task_content -> prio;
-// 			p_task_info -> ptask = task_content -> ptask;
-// 			
-// 		}
-// 		
-// 		if(task_content == NULL)
-// 		{
-// 			return(OS_R_NOK);
-// 		}
-// 		
-// 		return OS_R_OK;
 }
 /* end of file */
  
