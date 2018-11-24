2498f985b78208f31b944f1901ca26f5fcd6d5ab
Vyom Malhotra <v9malhot@uwaterloo.ca>
2018-11-24 18:10:54 -0500

    mem_init functions created

--- lab4/starter/mem.c
+++ lab4/starter/mem.c
@@ -12,7 +12,19 @@
 
 /* defines */
 
+struct memory_list
+{
+	size_t * address;
+	size_t space;
+	int allocated; //0 = unallocated, 1 = allocated
+
+	struct list * previous;
+	struct list * next;
+};
+
 /* global variables */
+struct memory_list best_memory;
+struct memory_list worst_memory;
 
 
 /* Functions */
@@ -20,25 +32,56 @@
 /* memory initializer */
 int best_fit_memory_init(size_t size)
 {
-
 	// To be completed by students
-	// You call malloc once here to obtain the memory to be managed.
-	return 0;
+	if(size < sizeof(struct memory_list))
+	{
+		return -1;
+	}
 
+	best_memory.address = malloc(size);
+	best_memory.space = size;
+	best_memory.allocated = 0;
+	best_memory.previous = NULL;
+	best_memory.next = NULL;
+
+	if(best_memory.address == NULL)
+	{
+		return -1;
+	}
+
+	return 0;
 }
 
 int worst_fit_memory_init(size_t size)
 {
 
 	// To be completed by students
-	// You call malloc once here to obtain the memory to be managed.
-	return 0;
+	if(size < sizeof(struct memory_list))
+	{
+		return -1;
+	}
 
+	worst_memory.address = malloc(size);
+	worst_memory.space = size;
+	worst_memory.allocated = 0;
+	worst_memory.previous = NULL;
+	worst_memory.next = NULL;
+
+	if(worst_memory.address == NULL)
+	{
+		return -1;
+	}
+
+	return 0;
 }
 
 /* memory allocators */
 void *best_fit_alloc(size_t size)
 {
+	//reduce size of node
+
+	//create new next node
+	
 	// To be completed by students
 	return NULL;
 }
